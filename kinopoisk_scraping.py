a97b87baa2bfd024fa509fc0d1dd97cedca45015
Пестерева Лиана Сергеевна <lspestereva@edu.hse.ru>
2025-03-11 01:57:04 +0300

    Now this file adds info from movie list to the SQLite database film.db

--- kinopoisk_scraping.py
+++ kinopoisk_scraping.py
@@ -1,17 +1,27 @@
-from bs4 import BeautifulSoup
-import sqlite3
+
 import requests
 import json
+import sqlite3
+import os
+import dotenv
+from dotenv import load_dotenv
+#from src.preprocess_text import PreprocessText
 
-TOKEN = "4FV4SHD-7MPMSTF-HZPVTKM-JBYWWVG"
+connection = sqlite3.connect("film.db")
+cursor = sqlite = connection.cursor()
+
+load_dotenv()
+
+
+TOKEN = os.dotenv("TOKEN")
+
+if not TOKEN: raise ValueError("Token not found")
 
 headers = {
     "X-API-KEY": f"{TOKEN}",
     "accept": "application/json"
 }
 
-seen_urls = set()
-links = []
 
 def save_json(name, data) :
     with open(name, 'w') as json_file:
@@ -22,12 +32,18 @@ def save_json(name, data) :
 def get_reviews(movie_id):
     review_url = f"https://api.kinopoisk.dev/v1.4/review?page=1&limit=10&selectFields=id&selectFields=review&selectFields=type&movieId={movie_id}"
     response = requests.get(review_url, headers = headers)
+    return response.json()
 
-    save_json("reviews"+str(movie_id)+".json", response)
 
-
+#gets movie info from json and puts it into database
 def get_movie_info(movie):
     id_ = movie['id']
+
+    cursor.execute("SELECT * FROM films WHERE ID = ?", (id_,))
+    if cursor.fetchone():
+        print(f"id {id_} already exists")
+        return
+
     rating = movie['rating']['kp']
     name = movie['name']+ ' (' + str(movie['year']) + ')'
     is_series = movie['isSeries']
@@ -35,21 +51,67 @@ def get_movie_info(movie):
         duration = movie['totalSeriesLength']
     else:
         duration = movie['movieLength']
-    genres=[]
-    for i in movie['genres']:
-        genres.append(i['name'])
+    genres = json.dumps([g['name'] for g in movie.get('genres', [])], ensure_ascii=False)
     age_rating = movie["ageRating"]
 
-    get_reviews(id_)
+    print(name)
+    reviews_data = get_reviews(id_)
+    reviews = json.dumps(reviews_data.get('docs', []), ensure_ascii=False)
+
+
+    cursor.execute('''
+                INSERT INTO films 
+                (ID, NAME, YEAR, RATING, IS_SERIES, GENRES, REVIEWS, AGE_RATING, DURATION)
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+            ''', (
+        id_,
+        name,
+        rating,
+        is_series,
+        json.dumps(genres, ensure_ascii=False),
+        reviews,
+        age_rating,
+        duration
+    ))
 
 
 
-def go_through_movie_list():
-    with open("movie_list.json", "r", encoding='utf-8') as file:
+
+def go_through_movie_list(file_name):
+    with open(file_name, "r", encoding='utf-8') as file:
         movie_list = json.load(file)
-    for i in range(0, 10):
+
+    for i in range(100, 186):
         get_movie_info(movie_list['docs'][i])
 
 
 if __name__=="__main__":
-    #go_through_movie_list()
+
+    try:
+        print('db init...')
+
+        connection.execute('''
+            CREATE TABLE IF NOT EXISTS films
+            (ID INTEGER PRIMARY KEY,
+            NAME TEXT,
+            RATING REAL,
+            IS_SERIES INTEGER,
+            GENRES TEXT,
+            REVIEWS TEXT,
+            AGE_RATING INTEGER,
+            DURATION INTEGER
+            );
+        ''')
+
+        go_through_movie_list("movie_list.json")
+
+        connection.commit()
+        cursor.close()
+
+    except sqlite3.Error as e:
+        print('Error occured - ', e)
+
+    finally:
+        if connection:
+            connection.close()
+            print("sqlite connection closed")
