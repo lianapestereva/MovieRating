c39a6b5076f3645a4002af09316468c2c4f8b92b
Пестерева Лиана Сергеевна <lspestereva@edu.hse.ru>
2025-06-13 00:09:03 +0300

    Added train_detection_model.py. Trained absa model, success rate on test data is 75%

--- src/absa/train_absa_model.py
+++ src/absa/train_absa_model.py
@@ -1,17 +1,22 @@
 from datasets import load_dataset
 from transformers import AutoTokenizer, AutoModelForSequenceClassification, TrainingArguments, Trainer
+from transformers import EarlyStoppingCallback
 import numpy as np
 import evaluate
 from aspect_data import label_map
 
+
+
 dataset = load_dataset("csv", data_files={"train": "movie_train_absa/train.csv", "test":"data/testing/test_absa.csv"})
 
 num_labels = len(label_map)
-
 tokenizer = AutoTokenizer.from_pretrained("DeepPavlov/rubert-base-cased")
-model = AutoModelForSequenceClassification.from_pretrained("DeepPavlov/rubert-base-cased", num_labels=num_labels, from_flax=True)
-for param in model.base_model.parameters():
-    param.requires_grad = False
+model = AutoModelForSequenceClassification.from_pretrained(
+    "DeepPavlov/rubert-base-cased",
+    num_labels=num_labels,
+    ignore_mismatched_sizes=True
+)
+
 
 print("loaded the model...")
 
@@ -20,44 +25,52 @@ def encode_labels(example):
     example["label"] = label_map[aspect_sentiment]
     return example
 
-dataset = dataset.map(encode_labels, remove_columns=["aspect", "sentiment"])
 
 def tokenize(examples):
-    return tokenizer(examples["sentence"], padding="max_length", truncation=True)
+    texts = [f"{sentence} [SEP] {aspect}" for sentence, aspect in zip(examples["sentence"], examples["aspect"])]
+    return tokenizer(texts, padding="max_length", truncation=True, max_length=32, return_special_tokens_mask=True)
 
 dataset = dataset.map(tokenize, batched=True)
+dataset = dataset.map(encode_labels)
+dataset = dataset.remove_columns(["sentence", "aspect", "sentiment"])
 
 columns_to_keep = ["input_ids", "attention_mask", "label"]
-dataset = dataset.remove_columns([col for col in dataset.column_names if col not in columns_to_keep])
+for split in dataset.keys():
+    cols_to_remove = [col for col in dataset[split].column_names if col not in columns_to_keep]
+    dataset = dataset.remove_columns(cols_to_remove)
+train_test_split = dataset["train"].train_test_split(test_size=0.1, seed=42)
 
 
-full_train_dataset = dataset["train"]
-train_test_split = full_train_dataset.train_test_split(test_size=0.1, seed=42)
 train_dataset = train_test_split["train"]
 eval_dataset = train_test_split["test"]
 test_dataset = dataset["test"]
 
 print("split the data...")
 
-metric = evaluate.load("accuracy")
+metric_acc = evaluate.load("accuracy")
+metric_f1 = evaluate.load("f1")
 
 def compute_metrics(eval_pred):
     logits, labels = eval_pred
     predictions = np.argmax(logits, axis=-1)
-    return metric.compute(predictions=predictions, references=labels)
+    return {
+        "accuracy" : metric_acc.compute(predictions=predictions, references=labels)["accuracy"],
+        "f1_macro" : metric_f1.compute(predictions=predictions, references=labels, average="macro")['f1']
+    }
 
 args = TrainingArguments(
     output_dir="absa_model",
-    evaluation_strategy="epoch",
+    eval_strategy="epoch",
     save_strategy="epoch",
     logging_strategy="epoch",
     learning_rate=2e-5,
-    per_device_train_batch_size=8,
-    per_device_eval_batch_size=8,
-    num_train_epochs=3,
+    per_device_train_batch_size=32,
+    per_device_eval_batch_size=32,
+    num_train_epochs=7,
     seed=42,
     load_best_model_at_end=True,
     metric_for_best_model="accuracy",
+    greater_is_better=True,
     report_to="none"
 )
 
@@ -65,9 +78,10 @@ args = TrainingArguments(
 trainer = Trainer(
     model=model,
     args=args,
-    train_dataset=dataset["train"],
-    eval_dataset=dataset["test"],
-    compute_metrics=compute_metrics
+    train_dataset=train_dataset,
+    eval_dataset=eval_dataset,
+    compute_metrics=compute_metrics,
+    callbacks=[EarlyStoppingCallback(early_stopping_patience=2)]
 )
 
 trainer.train()
--- src/absa/train_detection_model.py
+++ src/absa/train_detection_model.py
@@ -1,14 +1,82 @@
 from transformers import AutoTokenizer, AutoModelForTokenClassification
-from transformers import Trainer, TrainingArguments
+from transformers import Trainer, TrainingArguments, DataCollatorForTokenClassification
+from transformers import EarlyStoppingCallback
+from datasets import load_dataset
+import numpy as np
+import evaluate
 
 
-model = AutoModelForTokenClassification.from_pretrained("DeepPavlov/rubert-base-cased", num_labels=3)  # B/I/O
+
+
+label_names = ["O", "B-PLOT", "I-PLOT", "B-ACTING", "I-ACTING", "B-PICTURE", "I-PICTURE", "B-HUMOR", "I-HUMOR"]
+tag2id = {tag: i for i, tag in enumerate(label_names)}
+id2tag = {i: tag for i, tag in enumerate(label_names)}
+
+model = AutoModelForTokenClassification.from_pretrained(
+    "DeepPavlov/rubert-base-cased",
+    num_labels=3,
+    id2label=id2tag,
+    label2id=tag2id
+
+)
 tokenizer = AutoTokenizer.from_pretrained("DeepPavlov/rubert-base-cased")
 
+print("loaded the model...")
+
+def tokenize_and_align_labels(examples):
+    tokenized_inputs= tokenizer(examples['tokens'], truncation=True, is_split_into_words=True)
+    labels=[]
+    for i, tags in enumerate(examples['tags']):
+        word_ids = tokenized_inputs.word_ids(batch_index=i)
+        label_ids=[]
+        previous_word_idx =None
+        for word_idx in word_ids:
+            if word_idx is None:
+                label_ids.append(-100)
+            elif word_idx!= previous_word_idx:
+                label_ids.append(tag2id[tags[word_idx]])
+            else:
+                label_ids.append(-100)
+            previous_word_idx = word_idx
+        labels.append(label_ids)
+    tokenized_inputs["labels"] = labels
+    return tokenized_inputs
+
+
+dataset = load_dataset("json", data_files={"train": "../movie_train_absa/train_detection.json", "test":"data/testing/test_detection.json"})
+dataset = dataset.map(tokenize_and_align_labels, batched=True)
+
+train_test_split = dataset["train"].train_test_split(test_size=0.1, seed=42)
+train_dataset=train_test_split["train"]
+eval_dataset=train_test_split["test"]
+test_dataset = dataset["test"]
+
+print("split the data...")
+
+metric = evaluate.load("seqeval")
+
+def compute_metrics(p):
+    predictions, labels = p
+    predictions = np.argmax(predictions, axis=2)
+    true_labels = [[label_names[l] for l in label if l!=-100] for label in labels]
+    true_predictions = [[label_names[p] for (p, l) in zip(prediction, label) if l!=-100] for prediction, label in zip(predictions, labels)]
+    return metric.compute(predictions=true_predictions, references=true_labels)
+
+
+
+
 training_args = TrainingArguments(
-    output_dir="./aspect_model",
-    per_device_train_batch_size=16,
-    num_train_epochs=3,
+    output_dir="aspect_model",
+    per_device_train_batch_size=32,
+    per_device_eval_batch_size=32,
+    num_train_epochs=10,
+    eval_strategy='epoch',
+    save_strategy='epoch',
+    logging_strategy='epoch',
+    load_best_model_at_end=True,
+    metric_for_best_model='f1',
+    greater_is_better=True,
+    report_to="none"
 )
 
 trainer = Trainer(
@@ -16,6 +84,13 @@ trainer = Trainer(
     args=training_args,
     train_dataset=train_dataset,
     eval_dataset=eval_dataset,
+    data_collator=DataCollatorForTokenClassification(tokenizer),
+    compute_metrics=compute_metrics,
+    callbacks=[EarlyStoppingCallback(early_stopping_patience=4)]
 )
 
 trainer.train()
+print("the model is trained")
+results = trainer.evaluate(test_dataset)
+print(results)
+
