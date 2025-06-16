dfe6028fda95939c358cebc4def71ec016f22bdc
pinguinalice <63787015+pinguinalice@users.noreply.github.com>
2025-06-16 08:45:18 +0300

    Update app.py
    Final app.py

--- app.py
+++ app.py
@@ -1,13 +1,6 @@
-from app import app
-import os
-import json
+from app import create_app
+
+app = create_app()
 
 if __name__ == '__main__':
-    #os.makedirs('data', exist_ok=True)
-    
-    for file in ['user_answers.json', 'recommendations.json']:
-        if not os.path.exists(f'data/{file}'):
-            with open(f'data/{file}', 'w') as f:
-                json.dump([], f)
-    
     app.run(debug=True)
