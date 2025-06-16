0f9e9de6801925511c018428ea1784c9db48a111
Пестерева Лиана Сергеевна <lspestereva@edu.hse.ru>
2025-06-16 15:32:11 +0300

    minor update

--- src/absa/absa.py
+++ src/absa/absa.py
@@ -75,9 +75,4 @@ def analyze_review(review):
     return [sign(x[1]) for x in list(aspect_output.items())]
 
 
-if __name__ == "__main__":
-    with open(r"C:\Users\user\PycharmProjects\Movie Rating Project\data\testing\test_absa.csv", encoding="utf-8") as f:
-        s = f.readline()
-        for i in range(20):
-            s = f.readline().split(",")[0]
-            print(analyze_review(s))
+
