6e64069bd81b6c2f210721b83d7c2be28bd5e676
pinguinalice <63787015+pinguinalice@users.noreply.github.com>
2025-06-16 08:51:30 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -5,7 +5,7 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../data/film.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../collect-data/film.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
